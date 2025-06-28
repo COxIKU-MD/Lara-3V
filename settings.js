3bab0a415dd2bb00a1eba28caa39928caa7c0549
COxIKU <sathsarashiwanga22@gmail.com>
2025-06-28 18:03:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "YOUR_SESSION_ID", // Add your session id
-PREFIX: process.env.PREFIX || ".",
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
-DEV: process.env.DEV || "94779062397",
+SESSION_ID: process.env.SESSION_ID || "3ZAjUSKC#b_jTkY_3x-lfUclO4QxdJgr6fTWPpekaEzPY-IlKpTs", // Add your session id
+PREFIX: process.env.PREFIX || "•",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94757624137",
+DEV: process.env.DEV || "94757624137",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
